055a27e1a251dc6036bc26225a9de2cb1e34155d
Ronda49 <sudharshanslaptop@gmail.com>
2025-10-28 07:46:40 +0530

    Add CI pipeline with linting, testing, and Trivy scan

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ from fastapi import FastAPI, HTTPException
 app = FastAPI()
 
 # Fake in-memory database
-fake_db = {
-    1: {"name": "Alice", "age": 25},
-    2: {"name": "Bob", "age": 30}
-}
+fake_db = {1: {"name": "Alice", "age": 25}, 2: {"name": "Bob", "age": 30}}
+
 
 @app.get("/")
 def root():
     return {"message": "Welcome to FakeDB API"}
+
 
 @app.get("/users/{user_id}")
 def get_user(user_id: int):
     if user_id in fake_db:
         return fake_db[user_id]
     raise HTTPException(status_code=404, detail="User not found")
+
 
 @app.post("/users/{user_id}")
 def add_user(user_id: int, user: dict):
